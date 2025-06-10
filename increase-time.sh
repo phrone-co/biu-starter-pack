9a5f1156d9e7d410f2cbcf6772dea34defe1c134
wikiyodo <wikiyodo@gmail.com>
2025-06-10 05:45:11 +0100

    modify to contain increased time

---
+++ increase-time.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Navigate to the directory where your script is located
+cd src
+
+# Execute the Node.js script
+# The `node` command runs your JavaScript file
+node increase-time.js
